c63aacc03215eab836952bf5025ad5ef04952acd
yuryshev <stanislav.yuryshev@gmail.com>
2024-11-16 03:40:50 +0200

    Add HttpClient

--- HelperServices/ExchangesHealthCheck.Functions/Program.cs
+++ HelperServices/ExchangesHealthCheck.Functions/Program.cs
@@ -9,6 +9,7 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddHttpClient();
 
         services.AddScoped<IBinanceSpotAdapter, BinanceSpotAdapter>();
     })
